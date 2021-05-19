fc0743433e6f28829cf8470bb2f5ddb940c58a3d
Emil Andersson <e.andersson22@gmail.com>
2021-05-19 21:47:42 +0200

    added axios

--- project-sel/src/userSearch.js
+++ project-sel/src/userSearch.js
@@ -1,22 +1,40 @@
-// HÄR SKRIVS ALLT :))))))))))))))))
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import Axios from "axios";
 import "./App.css";
 
 export default function UserSearch() {
-    const [userResult, userInfo ] = useState([]);
+  const [userInfo, setUserInfo] = useState([]);
+  const apiKey = "RGAPI-184476c9-694b-47a1-bcfe-7428ad9ad198";
+  const inputRef = useRef();
 
-    const inputRef = useRef();
+  const apiCall = () => {
+    Axios.get(
+      `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${inputRef.current.value}?api_key=${apiKey}`
+    ).then((response) => {
+      console.log(response);
+    });
+  };
 
-    
-
-
-return (
-<div>   
-    <form class="form-inline my-2 my-lg-0 d-block text-center" id="search-bar">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" ref={inputRef}></input>
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>   
-</div>
-
-);
+  return (
+    <div>
+      <form
+        className="form-inline my-2 my-lg-0 d-block text-center"
+        id="search-bar"
+      >
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          ref={inputRef}
+        />
+        <input
+          type="button"
+          className="btn btn-outline-success my-2 my-sm-0"
+          value="Search"
+          onClick={apiCall}
+        />
+      </form>
+    </div>
+  );
 }
