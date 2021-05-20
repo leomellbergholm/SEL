d055114cf9591d12f7078f92b70e042ac3fe2ca1
Emil Andersson <e.andersson22@gmail.com>
2021-05-20 10:10:11 +0200

    api call done

--- project-sel/src/userSearch.js
+++ project-sel/src/userSearch.js
@@ -4,17 +4,44 @@ import "./App.css";
 
 export default function UserSearch() {
   const [userInfo, setUserInfo] = useState([]);
-  const apiKey = "RGAPI-184476c9-694b-47a1-bcfe-7428ad9ad198";
+  const [masteryInfo, setMasteryInfo] = useState([]);
+  const apiKey = "RGAPI-260f5ec1-aeab-4366-b948-fb940416c649";
   const inputRef = useRef();
+  const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
 
   const apiCall = () => {
     Axios.get(
-      `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${inputRef.current.value}?api_key=${apiKey}`
+      `${corsAnywhere}https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${inputRef.current.value}?api_key=${apiKey}`
     ).then((response) => {
-      console.log(response);
+      if (response.status == 200) {
+        const summonerId = response.data.id;
+        console.log(response);
+        setUserInfo([
+          {
+            summonerLvl: response.data.summonerLevel,
+            summonerName: response.data.name,
+            summonerIcon: response.data.profileIconId,
+          },
+        ]);
+        inputRef.current.value = "";
+
+        Axios.get(
+          `${corsAnywhere}https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${summonerId}?api_key=${apiKey}`
+        ).then((response) => {
+          console.log(response);
+          setMasteryInfo([
+            {
+              championId: response.data[0].championId,
+              championPoints: response.data[0].championPoints,
+              championLvl: response.data[0].championLevel,
+            },
+          ]);
+        });
+      }
     });
   };
-
+  console.log(masteryInfo);
+  console.log(userInfo);
   return (
     <div>
       <form
