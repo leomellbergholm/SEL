a312c721ea350951470b405d06cc8512d793f33e
SamuelAhlberg <samuel_ahlberg@hotmail.com>
2021-05-24 20:25:18 +0200

    Favorit

--- project-sel/src/favoriteList.js
+++ project-sel/src/favoriteList.js
@@ -1,6 +1,7 @@
-import React, { useState} from 'react';
-import Favorite from './favorite'
-import {userInfo} from "./userSearch";
+import React, { useRef, useState} from 'react';
+import Favorite from './favorite';
+import UserSearch from './userSearch';
+import {apiCall} from './userSearch';
 
 export default function FavoriteList(props) {
 
@@ -23,9 +24,9 @@ export default function FavoriteList(props) {
 
     function addFavorite(){
         let new_favorites = {
-            name: props.item.summonerName,
-            icon: props.item.summonerIcon,
-            lvl: props.item.summonerLvl
+            name: props.item[0].summonerName,
+            icon: props.item[0].summonerIcon,
+            lvl: props.item[0].summonerLvl
         };
 
         let favoritesList = loadFavorites();
@@ -33,17 +34,24 @@ export default function FavoriteList(props) {
 
         localStorage.setItem("favorites", JSON.stringify(favoritesList));
         setFavorites(favoritesList);
+
+        console.log(new_favorites)
         
     }
-
+    
     console.log(favoriteList)
+
+    function loadProfile(name){
+        
+        props.apiCall(name)
+    }
 
     return(
         <div className="card m-3 p-2" style={{ width: "15rem", opacity: "80%", float: "right"}}>
             <h3>Favorites</h3>
             <button className ="btn btn-success mt-3" value="Lägg till favorit"  onClick={() => {addFavorite()}} >Add to favorites</button>
             <ul className="favorite-list" style={{padding:0}}>
-                { favoriteList.map(favorite => <Favorite key={favorite.name} item={favorite}/>) }
+                { favoriteList.map(favorite => <Favorite key={favorite.name} item={favorite} loadProfile={loadProfile}/>) }
             </ul>
         </div>
     )
